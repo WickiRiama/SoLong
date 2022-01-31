aec3dea9b551ee5393a7e850c3a85656b3a68be6
mriant <mriant@DESKTOP-MK2220C.localdomain>
2022-01-31 17:25:39 +0100

    add error message and end of loop

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 11:31:55 by mriant            #+#    #+#             */
-/*   Updated: 2022/01/31 14:43:22 by mriant           ###   ########.fr       */
+/*   Updated: 2022/01/31 17:22:42 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ int	close(int keycode, t_vars *vars)
 	if (keycode == 65307)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
-		exit(0);
+		vars->win = NULL;
+		mlx_loop_end(vars->mlx);
 	}
 	return (0);
 }
@@ -32,11 +33,12 @@ int	close(int keycode, t_vars *vars)
 int	closex(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
-	exit(0);
+	vars->win = NULL;
+	mlx_loop_end(vars->mlx);
 	return (0);
 }
 
-int resize(t_vars *vars)
+int	resize(t_vars *vars)
 {
 	(void) vars;
 	printf("Not possible\n");
@@ -50,12 +52,22 @@ int	main(void)
 	vars.mlx = mlx_init();
 	if (!(vars.mlx))
 	{
-		printf("MLX not initialized\n");
-		return (0);
+		printf("Error\nCould'nt initialize MLX\n");
+		return (-1);
 	}
 	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	if (!(vars.win))
+	{
+		printf("Error\nCould'nt initialize window\n");
+		mlx_destroy_display(vars.mlx);
+		free(vars.mlx);
+		return (-1);
+	}
 	mlx_key_hook(vars.win, close, &vars);
 	mlx_hook(vars.win, 17, 0, closex, &vars);
 	mlx_hook(vars.win, 25, 0, resize, &vars);
 	mlx_loop(vars.mlx);
+	mlx_destroy_display(vars.mlx);
+	free(vars.mlx);
+	return (0);
 }
