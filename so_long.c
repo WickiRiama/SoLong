2628da1c1e65824164baad70654c639c7bbab3dc
Margaux RIANT <mriant@e2r13p7.clusters.42paris.fr>
2022-02-01 12:32:35 +0100

    put pix into img into screen

--- so_long.c
+++ so_long.c
@@ -14,9 +14,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
 typedef struct s_vars {
 	void	*mlx;
 	void	*win;
+	t_img	img;
 }				t_vars;
 
 int	close(int keycode, t_vars *vars)
@@ -32,16 +42,58 @@ int	close(int keycode, t_vars *vars)
 
 int	closex(t_vars *vars)
 {
+	mlx_destroy_image(vars->mlx, vars->img.mlx_img);
 	mlx_destroy_window(vars->mlx, vars->win);
 	vars->win = NULL;
 	mlx_loop_end(vars->mlx);
 	return (0);
 }
 
-int	resize(t_vars *vars)
+void	img_pix_put(t_img *img, int x, int y, int color)
 {
-	(void) vars;
-	printf("Not possible\n");
+	char    *pixel;
+	int		i;
+
+	i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+void	render_line(t_img *img, int y, int color)
+{
+	int	i;
+	int	j;
+
+		i = 0;
+		while (i < 640)
+		{
+			j = 0;
+			while (j < 480)
+			{	
+				if (j == y)
+					img_pix_put(img, i, j, color);
+				j ++;	
+			}
+			i ++;
+		}
+}
+
+int	render(t_vars *vars)
+{
+	if (vars->win)
+	{
+		render_line(&vars->img, 50, 0xFF0000);
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->img.mlx_img, 0, 0);
+	}
 	return (0);
 }
 
@@ -63,9 +115,11 @@ int	main(void)
 		free(vars.mlx);
 		return (-1);
 	}
+	vars.img.mlx_img = mlx_new_image(vars.mlx, 640, 480);
+	vars.img.addr = mlx_get_data_addr(vars.img.mlx_img, &vars.img.bpp, &vars.img.line_len, &vars.img.endian);
 	mlx_key_hook(vars.win, close, &vars);
+	mlx_loop_hook(vars.mlx, render, &vars);
 	mlx_hook(vars.win, 17, 0, closex, &vars);
-	mlx_hook(vars.win, 25, 0, resize, &vars);
 	mlx_loop(vars.mlx);
 	mlx_destroy_display(vars.mlx);
 	free(vars.mlx);
